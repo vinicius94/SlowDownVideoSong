617f17a473fd5e35df275dd37ab1cd9e9a489292
Vinicius de Souza Monteiro <viniciusmonteiro94@gmail.com>
2024-11-26 19:03:11 -0300

    Update main.js
    Keywords added to search
    Video description added to search
    New events added to listen

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const CONFIG = {
         video: 'video',
         speedButton: '#efyt-speed',
         videoTitle: '#title.ytd-watch-metadata',
+        videoDescription: '#description.ytd-watch-metadata',
         gamingLink: '#always-shown a#endpoint-link[href*=gaming]',
         scrollContainer: '#shelf-container > #scroll-container > #items'
     },
-    musicPatterns: /music|song|official|mv|lyric/i,
+    musicPatterns: /music|song|official|mv|lyric|playlist|radio/i,
     eventTypes: {
         normal: 'contextmenu',
         speedup: 'click'
@@ -17,6 +18,7 @@ const CONFIG = {
 function isMusicVideo() {
     const pageTitle = document.title.toLowerCase();
     const videoTitle = document.querySelector(CONFIG.selectors.videoTitle)?.textContent.toLowerCase();
+    const videoDescription = document.querySelector(CONFIG.selectors.videoDescription)?.textContent.toLowerCase();
     const isGamingVideo = document.querySelector(CONFIG.selectors.gamingLink);
     // Container in the bottom of video description listing the songs played in the video
     const scrollContainer = document.querySelector(CONFIG.selectors.scrollContainer);
@@ -24,6 +26,7 @@ function isMusicVideo() {
     return (
         CONFIG.musicPatterns.test(pageTitle) ||
         CONFIG.musicPatterns.test(videoTitle) ||
+        CONFIG.musicPatterns.test(videoDescription) ||
         (!isGamingVideo && scrollContainer?.childElementCount === 1)
     );
 }
@@ -71,6 +74,16 @@ function initialize() {
 
     console.info('Player found, setting up events');
     document.addEventListener('yt-page-data-updated', changeVideoSpeed);
+
+    if (document.querySelector(CONFIG.selectors.videoTitle))
+    {
+        const titleObserver = new MutationObserver(changeVideoSpeed);
+        titleObserver.observe(document.querySelector(CONFIG.selectors.videoTitle), { subtree: true, childList: true });
+
+        //This timeout is to ensure that we will be able to change the video speed
+        videoPlayer.addEventListener('play', () => setTimeout(changeVideoSpeed, 1000));
+    }
+
     changeVideoSpeed();
 }
 
